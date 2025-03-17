756aaf1e29ec9f340973123026274850429bd8f4
DikxonGDA <info.guardianesdelasfalto@gmail.com>
2025-03-17 12:10:10 +0100

    Added files via Greta

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// Obtén el nombre del repositorio del package.json
+const repository = process.env.npm_package_name;
+
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? `/${repository}/` : './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
-   build: {
+  build: {
     outDir: 'dist',
     sourcemap: true
-  },
+  }
 });
